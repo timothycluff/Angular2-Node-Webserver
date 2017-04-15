ba35f1321d359f8e3f82912b233dc085b208f1b4
tim.cluff <tim.cluff@fishbowlinventory.com>
2017-04-14 23:59:07 -0600

    Finallyyyyyy a working webserver on angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdInputModule,
+         MdCheckboxModule,
+         MdMenuModule,
+         MdSidenavModule,
+         MdToolbarModule,
+         MdListModule,
+         MdCardModule,
+         MdTabsModule,
+         MdButtonModule,
+         MdIconModule,
+         MdProgressSpinnerModule,
+         MdProgressBarModule,
+         MdDialogModule,
+         MdSnackBarModule
+} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdInputModule,
+    MdCheckboxModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdListModule,
+    MdCardModule,
+    MdTabsModule,
+    MdButtonModule,
+    MdIconModule,
+    MdProgressSpinnerModule,
+    MdProgressBarModule,
+    MdDialogModule,
+    MdSnackBarModule,
+    FlexLayoutModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
